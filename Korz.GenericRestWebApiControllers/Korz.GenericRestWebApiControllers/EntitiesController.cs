9ec238036e81343c0bd285701ed3077bc3cdbc66
Brian Korzynski <korzynski@live.com>
2013-07-09 00:32:51 -0400

    updated nuget package

---
+++ Korz.GenericRestWebApiControllers/Korz.GenericRestWebApiControllers/EntitiesController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using GenericRavenDataInterfaces;
+
+namespace GenericRestWebApiControllers
+{
+    public abstract class EntitiesController<TEntity, TEntityCriteria, TEntityDataFactory> : ApiController
+        where TEntity : IEntity
+        where TEntityCriteria : ISingleCriteriaEntity
+        where TEntityDataFactory : IDataFactory<TEntity, TEntityCriteria>
+    {
+        private readonly TEntityDataFactory _factory;
+
+        protected EntitiesController(TEntityDataFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public IEnumerable<TEntity> Get()
+        {
+            return _factory.Load();
+        }
+
+        public IEnumerable<TEntity> Get([FromBody]TEntityCriteria criteria)
+        {
+            return _factory.Load(criteria);
+        }
+    }
+}
---
+++ Korz.GenericRestWebApiControllers/Korz.GenericRestWebApiControllers/EntityController.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Http;
+using GenericRavenDataInterfaces;
+
+namespace GenericRestWebApiControllers
+{
+    public abstract class EntityController<TEntity, TEntityCriteria, TEntityDataFactory> : ApiController
+        where TEntity : IEntity
+        where TEntityCriteria : ISingleCriteriaEntity
+        where TEntityDataFactory : IDataFactory<TEntity, TEntityCriteria>
+    {
+        private readonly TEntityDataFactory _factory;
+
+        protected EntityController(TEntityDataFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public TEntity Get()
+        {
+            return _factory.New();
+        }
+
+        public TEntity Get(string id)
+        {
+            return _factory.Load(id);
+        }
+
+        public TEntity Post([FromBody]TEntity entity)
+        {
+            return _factory.Create(entity);
+        }
+
+        public TEntity Put(string id, [FromBody]TEntity entity)
+        {
+            return _factory.Update(id, entity);
+        }
+
+        public void Delete([FromUri]string id)
+        {
+            _factory.Delete(id);
+        }
+    }
+}
